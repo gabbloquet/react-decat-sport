890d798b5bb8a3bb1485ea0fdb3164c1154ae4e6
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-01-03 12:01:01 +0100

    :white_check_mark: url calls use url-search-params

--- src/components/sports-ranking/service.js
+++ src/components/sports-ranking/service.js
@@ -1,10 +1,17 @@
 import {getSport} from "../../services/sports";
 
+function extractSports(sportsData){
+  console.log(sportsData);
+  return sportsData
+    .filter(sport => !sport.error)
+    .map(({data, data: { attributes }}) => data && attributes);
+}
+
 function feedRelatedSports(sport, setRelatedSports){
   if(sport.related.length > 0){
     Promise.all(sport.related.map(sport => getSport(sport.data.id)))
       .then(responses => {
-        setRelatedSports(responses.map(({data, data: {attributes}}) => data && attributes));
+        setRelatedSports(extractSports(responses));
       });
   } else {
     setRelatedSports([]);
@@ -15,7 +22,7 @@ function feedChildren(sport, setChildren){
   if(sport.children.length > 0) {
     Promise.all(sport.children.map(child => getSport(child.data.id)))
       .then(responses => {
-        setChildren(responses.map(({data: {attributes}}) => attributes));
+        setChildren(extractSports(responses));
       });
   } else {
     setChildren([]);
--- src/utils/api-calls/helper.js
+++ src/utils/api-calls/helper.js
@@ -1,19 +1,10 @@
-/**
- * Recovery of basic data for the construction of an url with the base, the endpoint and the parameters if necessary,
- * it returns a usable url for API calls.
- * @param {URLConfig} urlConfig - The URL configuration object
- * @returns {string} - The URL built
- */
+import URLSearchParams from '@ungap/url-search-params'
+
 const urlConstructor = ({ baseUrl, apiEndpoint, queryParams }) => {
-  let urlConfig = `${baseUrl}${apiEndpoint}`;
+  let urlConfig = new URL(`${baseUrl}${apiEndpoint}`);
 
-  if (queryParams) {
-    const queryString = Object.keys(queryParams)
-      .map(key => key + '=' + queryParams[key])
-      .join('&');
-
-    urlConfig += `?${queryString}`;
-  }
+  if (queryParams)
+    urlConfig.search = new URLSearchParams(queryParams).toString()
 
   return urlConfig;
 };
