171d9d8cddc6fcc0fa6a23c812c9f05a4bf67de1
gabin.bloquet <gabin.bloquet@decathlon.com>
2020-02-14 11:41:40 +0100

    :pencil: Add React router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Homepage from "./containers/homepage";
 
 const App = () => {
   return (
-    <Homepage/>
+    <Router>
+      <Switch>
+        <Route path ='/' component={Homepage}/>
+      </Switch>
+    </Router>
   )
 };
 
